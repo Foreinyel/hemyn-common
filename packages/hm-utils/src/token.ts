3cc5477b43d3247f909b8c83395ba5d220b7d7b4
Foreinyel <Foreinyel@163.com>
2021-11-26 21:38:23 +0800

    feat: user token key

--- packages/hm-utils/src/token.ts
+++ packages/hm-utils/src/token.ts
@@ -1,11 +1,12 @@
+import { USER_TOKEN_KEY } from "./constants";
 import { localStorage } from "./storage";
 
-export const tokenKey = "auth-token";
+export const tokenKey = USER_TOKEN_KEY;
 
 export const setToken = (token: string) => {
-  localStorage.set(tokenKey, token);
+  localStorage.set(USER_TOKEN_KEY, token);
 };
 
 export const getToken = () => {
-  return localStorage.get(tokenKey);
+  return localStorage.get(USER_TOKEN_KEY);
 };
