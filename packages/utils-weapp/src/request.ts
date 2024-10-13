b89b9006d7c038d9bbd0a9c43022d1df202ec62d
Foreinyel <Foreinyel@163.com>
2024-10-13 17:07:46 +0800

    request

--- packages/utils-weapp/src/request.ts
+++ packages/utils-weapp/src/request.ts
@@ -1,23 +1,27 @@
 import Taro from "@tarojs/taro";
 
+interface Toast {
+  error?: (msg: string) => void;
+}
+
 let baseUrl: string;
+let toast: Toast | null = null;
 
 const setBaseUrl = (_baseUrl: string) => {
   baseUrl = _baseUrl;
-}
+};
 
 const getBaseUrl = () => {
   return baseUrl;
-}
+};
 
-export {
-  setBaseUrl, 
-  getBaseUrl
-}
+const setToast = (_toast: Toast) => {
+  toast = _toast;
+};
 
+export { setBaseUrl, getBaseUrl, setToast };
 
-
-export interface Reponse<T> {
+export interface Response<T> {
   code: number;
   data?: T;
   msg?: string;
@@ -26,12 +30,10 @@ export interface Reponse<T> {
 const request = async <T, U>(
   path: string,
   method: "GET" | "POST" | "PUT",
-  data: U
+  data?: U
 ) => {
-
   if (!baseUrl) {
-
-    throw new Error("baseUrl is empty")
+    throw new Error("baseUrl is empty");
   }
 
   const res: Taro.request.SuccessCallbackResult = await Taro.request({
@@ -40,30 +42,42 @@ const request = async <T, U>(
     data,
     header: {
       "content-type": "application/json",
-      Cookie: Taro.getStorageSync("cookieKey")
-    }
+      Cookie: Taro.getStorageSync("cookieKey"),
+    },
   });
   const cookie = res.header["set-cookie"] || res.header["Set-Cookie"];
   if (res && res.header && cookie) {
     Taro.setStorageSync("cookieKey", cookie); //保存Cookie到Storage
   }
-  return res.data as T;
+  return res.data as Response<T>;
 };
 
-export const rGet: <T>(path: string) => Promise<Reponse<T>> = async path => {
-  return await request(path, "GET", null);
+export const rGet = async <T, U>(path: string, data?: U) => {
+  const res = await request<T, U>(path, "GET", data);
+  if (res?.code === 0) {
+    return res?.data;
+  }
+  const err = res?.msg || "系统异常";
+  toast?.error?.(err);
+  throw err;
 };
 
-export const rPost: <T, U>(
-  path: string,
-  data: U
-) => Promise<Reponse<T>> = async (path, data) => {
-  return await request(path, "POST", data);
+export const rPost = async <T, U>(path: string, data?: U) => {
+  const res = await request<T, U>(path, "POST", data);
+  if (res?.code === 0) {
+    return res?.data;
+  }
+  const err = res?.msg || "系统异常";
+  toast?.error?.(err);
+  throw err;
 };
 
-export const rPut: <T, U>(
-  path: string,
-  data: U
-) => Promise<Reponse<T>> = async (path, data) => {
-  return await request(path, "PUT", data);
+export const rPut = async <T, U>(path: string, data?: U) => {
+  const res = await request(path, "PUT", data);
+  if (res?.code === 0) {
+    return res?.data;
+  }
+  const err = res?.msg || "系统异常";
+  toast?.error?.(err);
+  throw err;
 };
